049910ac2fe8919d1e1cd288ea384e9215ac16cd
DoubleZed <zintis.zilbers@gmail.com>
2022-12-07 22:34:40 +0100

    Adjusted Star 1 and added Star 2 solution

--- day1.py
+++ day1.py
@@ -17,8 +17,16 @@ groupSum = 0
 for x in cleanArray:
     if isinstance(x, int):
         groupSum = groupSum + x
+        if x == cleanArray[-1]: #If x is the last item in the list, sum the group
+            elfCalories.append(groupSum)
     else:
         elfCalories.append(groupSum)
         groupSum = 0
 
 print (max(elfCalories))
+
+### Part two ###
+
+elfCalories.sort(reverse=True)
+print(elfCalories[:3])
+print(sum(elfCalories[:3]))

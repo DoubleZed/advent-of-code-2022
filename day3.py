4581a3efea50975e0b81a6cd34e7f4ce1a7e762b
DoubleZed <zintis.zilbers@gmail.com>
2022-12-10 19:44:44 +0100

    Added solution to Part 1

--- day3.py
+++ day3.py
@@ -1,5 +1,21 @@
 import os
+import string
 ### Part one ###
 inputPath = os.path.join(os.getcwd(),"input","input_day3.txt")
 
 array = [line.rstrip() for line in open(inputPath,"r")]
+
+score = 0
+for x in array:
+    part1 = x[:len(x)//2]
+    part2 = x[len(x)//2:]
+ 
+    set_part1 = set(part1)
+    set_part2 = set(part2)
+
+    matched = set_part1 & set_part2
+
+    score += string.ascii_letters.find(matched.pop())+1
+
+print(score)
+
